2e383fa5e6211223c0f0f0d49fe184cd94693b69
Sebastian Latkolic <latkolicsebastian@gmail.com>
2021-04-13 20:30:46 +0300

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import MenuItem from '../menu-item/menu-item.component';
 import SECTIONS_DATA from './sections.data.js';
-import './directory.style.scss';
+import './directory.styles.scss';
 
 class Directory extends React.Component {
 	constructor() {
